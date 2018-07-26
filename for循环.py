72b0c98a9d9a7b9cda7e35fdb520f82263b68aaf
徐立 <78645574@qq.com>
2018-07-26 08:05:40 +0800

    init

--- for循环.py
+++ for循环.py
@@ -1,8 +1,26 @@
 # 1到5的范围
-index = 5
-for i in range(1, index):
-    print(i)
-    if i == len(range(index)) - 1:
-        print("循环结束")
-else:
-    print('for循环结束')
+# index = 5
+# for i in range(1, index):
+#     print(i)
+#     if i == len(range(index)) - 1:
+#         print("循环结束")
+# else:
+#     print('for循环结束')
+
+
+def release(lst):
+    fmp = []
+    for item in lst:
+        if isinstance(item, list):
+            tmp = release(item)
+            for item2 in tmp:
+                yield item2
+        else:
+            yield item
+
+
+# l = [1, 2, 3, 4, 5, [6], [7, 8, [9, [10]]]]
+# print(l)
+# lst = release(l)
+# print(lst)
+# print(list(lst))
