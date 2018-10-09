1b40779b1dec3ccfbe9898be9d1134a548c1882d
xuli <78645574@qq.com>
2018-10-09 13:17:04 +0800

    拼音

--- util/拼音2.py
+++ util/拼音2.py
@@ -1,6 +1,7 @@
-import pinyin
+from pinyin import pinyin
 # 汉字中的间隔符delimiter
 a = pinyin.get('杨强', format='strip', delimiter="")
 print(a)
-b = pinyin.get_initial('杨强')
+# 中间有空格
+b = pinyin.get_initial('杨w强')
 print(b.replace(' ', ''))
