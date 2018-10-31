63c00150fcb489fb148f3424ab494786c30d1993
徐立 <78645574@qq.com>
2018-10-31 22:48:42 +0800

    init

--- base/passwordTest.py
+++ base/passwordTest.py
@@ -8,7 +8,7 @@ def initPassWord():
     for x in range(8):
         result += str(random.randint(0, 9));
     return result
-for i in range(1000000):
+for i in range(10000000):
     password = initPassWord()  # 从字母和数字中随机取8位
     if password not in passwords:
         file.write(str(password) + "\n")
---
+++ base/string/zwf占位符.py
@@ -0,0 +1,6 @@
+# %s占位符 拼接字符串
+print('http://www.%shao123.com' % '这是占位符')
+who = 'you'
+she = 'she'
+a = f"I Love {she} {who}"
+print(a)
